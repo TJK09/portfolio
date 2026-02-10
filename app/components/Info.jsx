7fdde0ae784447ae33a9d9ba405a53f7609b7c1f
TJK09 <taimurjan241@gmail.com>
2026-02-10 14:19:29 +0500

    Added Introduction section

--- app/components/Info.jsx
+++ app/components/Info.jsx
@@ -3,8 +3,14 @@
 const Info = () => {
   return (
     <div>
-        <div className="flex bg-black text-white">
-            
+        <div className="flex flex-col p-20 bg-black text-white">
+            <div className="flex flex-col">
+                <h1 className="text-[30px] pb-2 font-bold">Introduction</h1>
+                <p className="pb-4 text-[15px] w-[80%] text-justify">Software Engineer with Interest in DevOps and Web Development (React and Django) or (MERN). Grip on Docker, Kubernetes, React Hooks, Props, Components and DRF(Django RestFramework). I've worked on MERN stack as well creating a web app. Familiar with CI/CD pipelines, containerization, and deploying applications using modern DevOps tools. Skilled in Git, cloud basics, debugging, and eager to gain hands-on DevOps and cloud deployment experience.</p>
+            </div>
+            <div className="flex pt-10 ">
+                <h1 className="font-semibold text-[15px] border-l-2 border-r-2 p-6 ">READ MORE</h1>
+            </div>
         </div>
     </div>
   )
