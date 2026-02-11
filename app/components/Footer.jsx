2e49ce7966cd0b2a8ed3e0b74ac529152b3dcda0
TJK09 <taimurjan241@gmail.com>
2026-02-11 14:24:19 +0500

    Completed Designing of the Website

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -15,10 +15,13 @@ const Footer = () => {
         <h1 className="text-[15px] font-bold">BACK TO TOP</h1>
       </div>
       <div className="flex items-center justify-center gap-5 mt-[50px]">
-        <button onClick={()=>window.open("")}><Image src={Facebook} alt="Facebook" className="text-white w-10 h-10" /></button>
+        <button onClick={()=>window.open("https://www.facebook.com/taimur.jan.141877")}><Image src={Facebook} alt="Facebook" className="text-white w-10 h-10" /></button>
         <button onClick={()=>window.open("https://www.instagram.com/khattak_taimur/")}><Image src={Instagram} alt="Instagram" className="text-white w-10 h-10" /></button>
-        <button onClick={()=>window.open("")}><Image src={linkedin} alt="Linkedin" className="text-white w-10 h-10" /></button>
+        <button onClick={()=>window.open("https://www.linkedin.com/in/taimur-jan-69518b297/")}><Image src={linkedin} alt="Linkedin" className="text-white w-10 h-10" /></button>
         <button onClick={()=>window.open("mailto:taimurjan241@gmail.com")}><Image src={email} alt="Email" className="text-white w-10 h-10" /></button>
+      </div>
+      <div className="flex justify-center items-center mt-10">
+        <p className="text-[15px] font-bold text-white">@2026 Taimur Jan. All rights Reserved</p>
       </div>
     </div>
   );
